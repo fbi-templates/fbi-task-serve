61f473e08411ecb9e89a2e3587553e71e9e81ef6
shawnnxiao <shawnnxiao@tencent.com>
2017-11-19 02:31:20 +0800

    change task to module

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const http = require('http')
 const Koa = require('koa')
 const serve = require('koa-static')
-const app = new Koa()
 
-let start = taskParams
+let startPort = taskParams
   ? (taskParams.p || 3000) * 1
   : ctx.options.server ? ctx.options.server.port : 3000
 
@@ -12,35 +11,38 @@ const host = ctx.options.server
   : 'localhost'
 const root = ctx.options.server ? ctx.options.server.root || '.' : '.'
 
-// serve static
-app.use(serve(root))
+function listen(app) {
+  return new Promise((resolve, reject) => {
+    let port = startPort
+    startPort += 1
+    const server = http.createServer(app.callback())
 
-// auto selected a valid port & start server
-function autoPortServer(cb) {
-  let port = start
-  start += 1
-  const server = http.createServer(app.callback())
-
-  server.listen(port, err => {
-    server.once('close', () => {
-      app.listen(port, err => {
-        if (err) {
-          ctx.log(err)
-          return
-        }
-        cb(port)
+    server.listen(port, err => {
+      server.once('close', () => {
+        app.listen(port, err => {
+          return err ? reject(err) : resolve(port)
+        })
       })
+      server.close()
     })
-    server.close()
-  })
-  server.on('error', err => {
-    ctx.logger.warn(`port ${port} is already in used`)
-    autoPortServer(cb)
+    server.on('error', err => {
+      ctx.logger.warn(`port ${port} is already in used.`)
+      listen(app)
+    })
   })
 }
 
-// listen
-autoPortServer(port => {
-  ctx.logger.success(`Server runing at http://${host}:${port}`)
-  ctx.logger.info(`Server root: ${root}`)
-})
+async function start() {
+  try {
+    const app = new Koa()
+    app.use(serve(root))
+
+    const port = await listen(app)
+    ctx.logger.success(`Server runing at http://${host}:${port}`)
+    ctx.logger.info(`Server root: ${root}`)
+  } catch (err) {
+    ctx.logger.error(err)
+  }
+}
+
+module.exports = start
