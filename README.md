3fb038f2868f45b6592d8441e538b10f94ecb8ae
neikvon <neikvon@hotmail.com>
2018-07-05 14:02:02 +0800

    1. replace `koa` with `express` 2. support directory listings

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # fbi-task-serve
-Static server using koa 2.
+Static server with directory listings.
 
 > This is a fbi task. If you haven't installed [fbi](https://github.com/AlloyTeam/fbi) yet, use the following command to install.
 >
@@ -8,6 +8,11 @@ Static server using koa 2.
 ## Requirements
 - `fbi v3.0+`
 - `node v7.6+`
+
+## Features
+
+- serve static files
+- directory listings
 
 ## Usage
 
@@ -24,6 +29,13 @@ $ cd path/to/any/directory
 $ fbi serve
 ```
 
+**Run with params**
+
+```bash
+$ fbi serve -p=9999
+$ fbi serve -port=9999
+```
+
 ## More
 - [Official templates](https://github.com/fbi-templates)
 - [`fbi` documentation](https://neikvon.gitbooks.io/fbi/content/)
@@ -32,6 +44,10 @@ $ fbi serve
 [MIT](https://opensource.org/licenses/MIT)
 
 ## Changelog
+
+- **2.0.0** (2018.07.05)
+  - replace `koa` with `express`
+  - support directory listings
 
 - **1.2.0** (2017.11.25)
   - init
--- index.js
+++ index.js
@@ -1,46 +1,27 @@
-const http = require('http')
-const Koa = require('koa')
-const serve = require('koa-static')
+const path = require('path')
+const express = require('express')
+const getPort = require('get-port')
+const serveIndex = require('serve-index')
 
-// Get task params
-const taskParams = ctx.task.getParams('serve')
-
-// Service start port
-let startPort = taskParams.port * 1 || 3000
-
-const host = ctx.options.server
-  ? ctx.options.server.host || 'localhost'
-  : 'localhost'
-const root = ctx.options.server ? ctx.options.server.root || '.' : '.'
-
-function listen(app) {
-  return new Promise((resolve, reject) => {
-    let port = startPort
-    startPort += 1
-    const server = http.createServer(app.callback())
-
-    server.listen(port, err => {
-      server.once('close', () => {
-        app.listen(port, err => {
-          return err ? reject(err) : resolve(port)
-        })
-      })
-      server.close()
-    })
-    server.on('error', err => {
-      ctx.logger.warn(`Port ${port} is already in use, trying ${startPort}...`)
-      resolve(listen(app))
-    })
-  })
-}
-
-async function start() {
+async function start () {
   try {
-    const app = new Koa()
-    app.use(serve(root))
-    const port = await listen(app)
-    ctx.logger.success(`Server runing at http://${host}:${port}`)
-    ctx.logger.log(`Server root: ${root}`)
+    const app = express()
+    const siteRoot = process.cwd()
+    const taskParams = ctx.task.getParams('serve')
+    const startPort = taskParams.port * 1 || taskParams.p * 1 || 3000
+    const port = await getPort({ port: startPort })
+
+    app.use(express.static(siteRoot))
+    app.use(
+      serveIndex(siteRoot, {
+        stylesheet: path.join(__dirname, './public/style.css')
+      })
+    )
+
+    await app.listen(port)
+
+    ctx.logger.log(`Server root: ${siteRoot}`)
+    ctx.logger.success(`Server runing at http://localhost:${port}`)
   } catch (err) {
     ctx.logger.error(err)
   }
